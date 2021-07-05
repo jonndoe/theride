b8d00f3acd0625017caa6e557d1446cc66bfd4e9
jonndoe <lukichevvasily@gmail.com>
2021-07-05 10:06:09 +0100

    Setup heroku 3

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,7 +4,8 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+#SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = 'KAJSDHFLAKSDFHLASDKFJASHLDFKJASHLDFKAJSDFASLKJDFHLASDKJFHLASDKFJHASLDKFJASHLDKFJHALSKDJFHLASKJDFHLASKJDFHLAKSJDFHLASKJDFHL'
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["theride.com"])
 
